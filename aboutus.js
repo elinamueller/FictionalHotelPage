0d58221b52ecfc11c90d45669cd59a67ce680fe1
elinoiz <81512565+elinamueller@users.noreply.github.com>
2022-05-11 10:20:12 +0200

    Delete aboutus.js

--- aboutus.js
+++
@@ -1,8 +0,0 @@
-//moon button dark mode
-const moonButton = document.getElementById("moonButton");
-
-function darkMode() {
-    const bodyElement = document.body;
-    bodyElement.classList.toggle("dark-mode");
-}
-moonButton.addEventListener("click", darkMode);
